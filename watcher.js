d6c0a886df2c6bfee22d7f303ad7c0e8133fb18e
Charlie Greenman <charie@razroo.com>
2018-01-01 14:43:16 -0500

    Add shelljs to app. Remove and re-order dependencies as deemed fit.

--- watcher.js
+++ watcher.js
@@ -1,6 +1,10 @@
 var chokidar = require('chokidar');
+var shell = require('shelljs');
+
+shell.exec('pdflatex main.tex');
 
 // One-liner for current directory, ignores .dotfiles
 chokidar.watch('*.tex').on('all', (event, path) => {
+  shell.exec('pdflatex main.tex');
   console.log(event, path);
 });
